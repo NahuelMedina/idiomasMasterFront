322616bfcf4029d1ac2ffa6ea684c576e46cc36c
Nahuel Medina <nahuelmedina2000@gmail.com>
2024-02-06 16:49:43 -0300

    remove cards de landing

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,9 +2,5 @@ import "../../Styles/styles.css";
 import { Cards } from "../Cards/Cards";
 
 export const Landing = () => {
-  return (
-    <div className="landing">
-      <Cards />
-    </div>
-  );
+  return <div className="landing"></div>;
 };
