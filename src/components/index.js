469ce5cd0532cf36516ec54caa90c1e5c31ab4ce
Nahuel Medina <nahuelmedina2000@gmail.com>
2024-02-06 21:27:46 -0300

    add login and register components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import { Navbar } from "./NavBar/Navbar";
 import { Cards } from "./Cards/Cards";
 import { Card } from "./Card/Card";
-export { Navbar, Card, Cards };
+import { Register } from "./Register/Register";
+import { Landing } from "../components/Landing/Landing";
+import { Login } from "./Login/Login";
+
+export { Navbar, Card, Cards, Register, Landing, Login };
