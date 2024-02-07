046cff8d5986de0295371d9f89ec0e664a891c61
Nahuel Medina <nahuelmedina2000@gmail.com>
2024-02-07 18:33:29 -0300

    add register validation y css en register y login

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -4,10 +4,12 @@ export const Navbar = () => {
   return (
     <div className="flex h-20 justify-between items-center text-white bg-black">
       <div className="flex justify-center w-1/2 items-center gap-20">
-        <img
-          className="w-32"
-          src="src\assets\logo\logo_idiomaster-removebg-preview.png"
-        ></img>
+        <Link to="/">
+          <img
+            className="w-32"
+            src="src\assets\logo\logo_idiomaster-removebg-preview.png"
+          ></img>
+        </Link>
         <div>
           <NavLink className="navButton">Sobre Nosotros</NavLink>
         </div>
@@ -20,7 +22,6 @@ export const Navbar = () => {
           </NavLink>
         </div>
       </div>
-<<<<<<< HEAD
       <div className="mr-20">
         {location.pathname !== "/register" && location.pathname !== "/login" ? (
           <Link to="/register" className="navButton">
@@ -29,18 +30,6 @@ export const Navbar = () => {
         ) : (
           <Link></Link>
         )}
-=======
-      <div>
-        <button className="navButton">Cursos</button>
-      </div>
-      <div>
-        <Link to='/home'>
-          <button className="navButton">Explorar</button>
-        </Link>
-      </div>
-      <div className="loginDiv">
-        <button className="navButton">Registrarse</button>
->>>>>>> 3f73ac229de72e66f06dae29e63c58a9d2a969dd
       </div>
     </div>
   );
