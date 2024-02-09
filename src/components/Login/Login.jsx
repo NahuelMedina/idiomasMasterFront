160b9d6be85e67eb67c92b15edcb1c5fa1691f18
Nahuel Medina <nahuelmedina2000@gmail.com>
2024-02-09 11:31:41 -0300

    cambios en manejo de errores en reducer, register y login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { getUser } from "../../redux/action/actions";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+
   const [states, setStates] = useState({
     email: "",
     password: "",
@@ -27,13 +31,18 @@ export const Login = () => {
     }
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getUser(states));
+  };
+
   return (
     <div className="w-screen h-screen bg-black text-white flex justify-center items-center animate-fade animate-once animate-ease-in">
       {console.log(states)}
       <div className="w-3/5 h-full">
         <img
-          className="h-screen"
-          src="https://pixabay.com/get/g353191db0b39416e871663e71b9a1dc47ca60875e023756ab5d29375c17606ca40f4db29117bd4dd158b0320998e13cb.jpg"
+          className="h-screen object-cover"
+          src="src\assets\fotos\image-login.jpg"
           alt=""
         />
       </div>
@@ -41,12 +50,15 @@ export const Login = () => {
         <h2 className="absolute top-32 right-40 text-3xl font-bold">
           Iniciar Sesion
         </h2>
-        <form className="h-screen w-full flex justify-center items-center flex-col bg-stone-900 rounded-lg text-lg pt-10 shadow-lg shadow-indigo-950 font-medium">
+        <form
+          onSubmit={handleSubmit}
+          className="h-screen w-full flex justify-center items-center flex-col bg-[#1F1F1F] rounded-lg text-lg pt-10 shadow-lg shadow-indigo-950 font-medium"
+        >
           <div className="flex flex-col gap-2 w-2/4 h-2/5 text-center items-center">
             <label htmlFor="email">Email</label>
             <input
               onChange={handleChange}
-              className="text-black rounded-md h-8 outline-none pl-1 focus:border-2 border-indigo-600 w-60"
+              className="text-black rounded-md h-8 outline-none pl-1 focus:border-2 border-[#1E68AD] w-60"
               name="email"
               placeholder="Email..."
               id="email"
@@ -55,7 +67,7 @@ export const Login = () => {
             <label htmlFor="password">Password</label>
             <input
               onChange={handleChange}
-              className="text-black rounded-md h-8 outline-none pl-1 focus:border-2 border-indigo-600 w-60"
+              className="text-black rounded-md h-8 outline-none pl-1 focus:border-2 border-[#1E68AD] w-60"
               name="password"
               placeholder="Constraseña..."
               id="password"
