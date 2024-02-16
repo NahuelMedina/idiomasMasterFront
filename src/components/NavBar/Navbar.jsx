d1e17687b7317b6986f539e66e3b823065aaafa1
GasparMon <imoncivaez@gmail.com>
2024-02-16 15:39:41 +0100

    update

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,28 +1,26 @@
-import { Link, useLocation } from "react-router-dom";
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { IoLogIn } from "react-icons/io5";
-import { PiStudentBold } from "react-icons/pi";
-import { FaDiscourse } from "react-icons/fa6";
+import { FaDiscourse } from "react-icons/fa";
 import { BsFillInfoSquareFill } from "react-icons/bs";
-
-import { FaUserGraduate } from "react-icons/fa";
-
-
-
+import { useAuth0 } from "@auth0/auth0-react";
 
 export const Navbar = () => {
-  const { isAuthenticated } = useAuth0(); // Obtiene las funciones de autenticación de Auth0
-  
-  const logout = () => {
+  const { isAuthenticated, logout } = useAuth0();
+ const navigate = useNavigate()
+  const location = useLocation();
+
+  const handleLogout = () => {
     if (window.gapi && window.gapi.auth2) {
       var auth2 = window.gapi.auth2.getAuthInstance();
       auth2.disconnect().then(function () {
         console.log("User disconnected.");
       });
     }
-  
-    setAuth(null);
+
+    logout({ returnTo: window.location.origin });
     localStorage.removeItem("auth");
-    history.push("/home");
+    navigate("/home");
   };
 
   return (
@@ -30,10 +28,7 @@ export const Navbar = () => {
       <div className="flex items-center justify-evenly w-[700px] h-full ">
         <Link to="/" className="h-full">
           <div className="flex items-center justify-center h-full w-[60px]">
-            <img
-              className="w-32"
-              src="src\assets\logo\logo4.png"
-            ></img>
+            <img className="w-32" src="src/assets/logo/logo4.png" alt="Logo" />
           </div>
         </Link>
 
@@ -44,7 +39,6 @@ export const Navbar = () => {
           </div>
         </Link>
 
-        
         <Link to="/home" className="h-full">
           <div className="flex items-center justify-evenly h-full w-[150px] transition-colors duration-300 ease-in-out border-b-4 border-black hover:border-white border-t-[4px]">
             <h1 className="text-[20px]">Cursos</h1>
@@ -53,40 +47,36 @@ export const Navbar = () => {
         </Link>
 
         {isAuthenticated && (
-          <>
-            <Link to="/createCourse" className="h-full">
-              <div className="flex items-center justify-center h-full w-[150px] transition-colors duration-300 ease-in-out border-b-4 border-black hover:border-white border-t-[4px]">
-                <h1>Crear Curso</h1>
-              </div>
-            </Link>
-          </>
+          <Link to="/createCourse" className="h-full">
+            <div className="flex items-center justify-center h-full w-[150px] transition-colors duration-300 ease-in-out border-b-4 border-black hover:border-white border-t-[4px]">
+              <h1>Crear Curso</h1>
+            </div>
+          </Link>
         )}
       </div>
+
       <Link to='/favorite'>
         <h1>Favoritos</h1>
       </Link>
       <Link to='/cart'>
         <h1>Carrito</h1>
       </Link>
-      {location.pathname !== "/register" && location.pathname !== "/login" ? (
+
+      {location.pathname !== "/register" && location.pathname !== "/login" && (
         <div className=" relative flex flex-row items-center justify-evenly w-[400px] h-full mr-[80px]">
-          <Link
-            to="/login"
-            className=" w-full h-full flex flex-row items-center"
-          >
+          <Link to="/login" className=" w-full h-full flex flex-row items-center">
             <div className="flex items-center justify-evenly h-full w-full transition-colors duration-300 ease-in-out border-b-4 border-black hover:border-white border-t-[4px]">
               <h1 className="text-[20px]">Ingresa</h1>
-              <IoLogIn  className="text-[40px]" />
+              <IoLogIn className="text-[40px]" />
             </div>
           </Link>
-          <Link to="/" onClick={() => logout()} className=" w-full h-full flex flex-row items-center">
+          <button onClick={handleLogout} className="w-full h-full flex flex-row items-center">
             <div className="flex items-center justify-evenly h-full w-full transition-colors duration-300 ease-in-out border-b-4 border-black hover:border-white border-t-[4px]">
-              <LogoutButton />
+              <h1>Cerrar Sesión</h1>
             </div>
-          </Link>
+          </button>
         </div>
       )}
     </div>
   );
 };
-
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -1,129 +1,140 @@
 import {
-    ALL_COURSES,
-    COURSE_DETAIL,
-    FILTERED_COURSES,
-    FILTER_LANGUAGE,
-    FILTER_LEVEL,
-    ORDER_PRICE,
-    SEARCH,
-    ADD_FAV,
-    SET_USER_DATA,
-    GET_USER_REQUEST,
-    GET_USER_SUCCESS,
-    GET_USER_FAILURE
+  ALL_COURSES,
+  COURSE_DETAIL,
+  FILTERED_COURSES,
+  FILTER_LANGUAGE,
+  FILTER_LEVEL,
+  ORDER_PRICE,
+  SEARCH,
+  ADD_FAV,
+  SET_USER_DATA,
+  GET_USER_REQUEST,
+  GET_USER_SUCCESS,
+  GET_USER_FAILURE,
+  ADMINPRODUCT,
 } from "../action/actiontypes";
 
 let initialState = {
-    courses: [],
-    coursesCopy: [],
-    courseDetail: [],
-    coursesName: [],
-    courseLanguage: '',
-    favorites: [],
-    allFavorites: [],
-    userData: "",
+  courses: [],
+  coursesCopy: [],
+  courseDetail: [],
+  coursesName: [],
+  courseLanguage: "",
+  favorites: [],
+  allFavorites: [],
+  userData: "",
+  loading: false,
+  error: null,
+  user: {
     loading: false,
-    error: null,
-    user: {
-        loading: false,
-      },
+  },
+  adminProduct: null,
 };
 
 const reducer = (state = initialState, { type, payload }) => {
-    switch (type) {
-        case ALL_COURSES:
-            return {
-                ...state,
-                courses: payload,
-                coursesCopy: payload, // Actualiza la copia de seguridad de los cursos
-            };
-        case COURSE_DETAIL:
-            
-            return {
-                ...state,
-                courseDetail: payload,
-            } 
-           
-        case FILTER_LANGUAGE:
-            const filteredByLanguage = state.coursesCopy.filter(course => course.language === payload);
-            return {
-                ...state,
-                courses: filteredByLanguage,
-                coursesName: filteredByLanguage,
-            };
-        case FILTER_LEVEL:
-            const filteredByLevel = state.coursesCopy.filter(course => course.level === payload);
-            return {
-                ...state,
-                courses: filteredByLevel,
-                coursesName: filteredByLevel,
-            };
-        case ORDER_PRICE:
-            if (payload === "default") {
-                return {
-                    ...state,
-                    courses: state.coursesCopy, // Revierte al estado inicial
-                };
-            }
-            const sortOrder = payload === "A" ? 1 : -1;
-            const sortedArray = [...state.courses].sort((a, b) => sortOrder * (a.price - b.price));
-            return {
-                ...state,
-                courses: sortedArray,
-                coursesName: sortedArray,
-            };
-        case SEARCH:
-            // Revierte al estado inicial antes de realizar la búsqueda
-            return {
-                ...state,
-                courses: state.coursesCopy,
-                coursesName: payload[0],
-                courseLanguage: payload[1],
-            };
+  switch (type) {
+    case ALL_COURSES:
+      return {
+        ...state,
+        courses: payload,
+        coursesCopy: payload, // Actualiza la copia de seguridad de los cursos
+      };
+    case COURSE_DETAIL:
+      return {
+        ...state,
+        courseDetail: payload,
+      };
 
-        case FILTERED_COURSES: 
-
+    case FILTER_LANGUAGE:
+      const filteredByLanguage = state.coursesCopy.filter(
+        (course) => course.language === payload
+      );
+      return {
+        ...state,
+        courses: filteredByLanguage,
+        coursesName: filteredByLanguage,
+      };
+    case FILTER_LEVEL:
+      const filteredByLevel = state.coursesCopy.filter(
+        (course) => course.level === payload
+      );
+      return {
+        ...state,
+        courses: filteredByLevel,
+        coursesName: filteredByLevel,
+      };
+    case ORDER_PRICE:
+      if (payload === "default") {
         return {
+          ...state,
+          courses: state.coursesCopy, // Revierte al estado inicial
+        };
+      }
+      const sortOrder = payload === "A" ? 1 : -1;
+      const sortedArray = [...state.courses].sort(
+        (a, b) => sortOrder * (a.price - b.price)
+      );
+      return {
+        ...state,
+        courses: sortedArray,
+        coursesName: sortedArray,
+      };
+    case SEARCH:
+      // Revierte al estado inicial antes de realizar la búsqueda
+      return {
+        ...state,
+        courses: state.coursesCopy,
+        coursesName: payload[0],
+        courseLanguage: payload[1],
+      };
+
+    case FILTERED_COURSES:
+      return {
+        ...state,
+        courses: payload,
+      };
+
+    case ADD_FAV:
+      const aux = [];
+      if (!aux.find((c) => c === payload)) {
+        aux.push(payload);
+      }
+
+      return {
+        ...state,
+        favorites: aux,
+        allFavorites: [...state.favorites, payload],
+      };
+
+    case GET_USER_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case GET_USER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        user: payload, // Cambiar action.payload por payload
+        error: null,
+      };
+    case GET_USER_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload, // Cambiar action.payload por payload
+      };
+
+    case ADMINPRODUCT:
+        return{
             ...state,
-            courses: payload
+            adminProduct: payload
         }
-        
-        case ADD_FAV:
-            const aux = []
-                if(!aux.find(c => c === payload)){
-                    aux.push(payload)
-                }
-            
-            return{
-                ...state,
-                favorites:aux,
-                allFavorites: [...state.favorites, payload]
-            }
 
-       
-                case GET_USER_REQUEST:
-                    return {
-                      ...state,
-                      loading: true,
-                      error: null
-                    };
-                    case GET_USER_SUCCESS:
-                        return {
-                          ...state,
-                          loading: false,
-                          user: payload, // Cambiar action.payload por payload
-                          error: null
-                        };
-                      case GET_USER_FAILURE:
-                        return {
-                          ...state,
-                          loading: false,
-                          error: payload // Cambiar action.payload por payload
-                        };
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
 
-export default reducer; 
+export default reducer;
