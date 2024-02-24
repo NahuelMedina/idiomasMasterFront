ed4fc0cf54c3fe880758b6127027b1a9f185040d
tomaspon <tomaspon11@gmail.com>
2024-02-24 20:45:42 -0300

    features(client/component - Auth0 minimal corrections)

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { store } from "./redux/store/store.js";
 import i18n from "./i18n.js";
