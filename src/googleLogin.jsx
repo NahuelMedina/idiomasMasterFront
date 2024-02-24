776a28eed3825d623b617a49e90f24d33dce5280
tomaspon <tomaspon11@gmail.com>
2024-02-24 19:31:12 -0300

    features(client/components - Auth0 corrections)

--- src/googleLogin.jsx
+++ src/googleLogin.jsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useDispatch } from 'react-redux';
-import { postThirdPartyUser } from './redux/action/actions'; // Importa la acción adecuada
+import { useDispatch } from "react-redux";
+import { postThirdPartyUser } from "./redux/action/actions";
 
 const LoginButton = () => {
-    const { loginWithRedirect } = useAuth0();
-    const dispatch = useDispatch();
+  const { loginWithRedirect } = useAuth0();
+  const dispatch = useDispatch();
 
-    const onSuccess = (userData) => {
-        console.log('Datos del usuario recibidos:', userData);
-        dispatch(postThirdPartyUser(userData));
-    };
+  const onSuccess = (userData) => {
+    console.log("Datos del usuario recibidos:", userData);
+    dispatch(postThirdPartyUser(userData));
+  };
 
-    return (
-        <div>
-            <button onClick={() => loginWithRedirect({ screen_hint: 'signup', connection: 'google-oauth2', onSuccess })}>
-                <img src="https://raw.githubusercontent.com/react-native-google-signin/google-signin/HEAD/img/signin-button.png" alt="" className="w-50 h-14" />
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <button
+        onClick={() =>
+          loginWithRedirect({
+            screen_hint: "signup",
+            connection: "google-oauth2",
+            onSuccess,
+          })
+        }
+      >
+        <img
+          src="https://raw.githubusercontent.com/react-native-google-signin/google-signin/HEAD/img/signin-button.png"
+          alt=""
+          className="w-50 h-14"
+        />
+      </button>
+    </div>
+  );
 };
 
 export default LoginButton;
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import { Auth0Provider } from '@auth0/auth0-react'
+import { Auth0Provider } from "@auth0/auth0-react";
 import { store } from "./redux/store/store.js";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider domain="idiomas-master.us.auth0.com" clientId="QKuJSniRtysK5OlPJy3muRNgxlcujinH" redirect_uri={window.location.origin}>
+      <Auth0Provider
+        domain="idiomas-master.us.auth0.com"
+        clientId="QKuJSniRtysK5OlPJy3muRNgxlcujinH"
+        authorizationParams={{ redirect_uri: window.location.origin }}
+      >
         <Provider store={store}>
           <App />
         </Provider>
